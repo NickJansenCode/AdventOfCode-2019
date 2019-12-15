d4bbc86ef4f9921cba308db4df387e023abaeb7d
NickJansenCode <nickjansen2497@gmail.com>
2019-12-15 02:05:37 -0500

    Solved Day 1 problems.

---
+++ AdventOfCode/Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The amount of fuel required is {Array.ConvertAll(System.IO.File.ReadAllLines("modules.txt"), x => int.Parse(x)).ToList().Sum(x => (x / 3) - 2)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode/Day1B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1B
+{
+    class Program
+    {
+
+        static int GetFuelRequirement(int module, int origin = 0)
+        {
+            Func<int, int> fuelRequired = x => (x / 3) - 2;
+            return fuelRequired(module) <= 0 ? origin : GetFuelRequirement(fuelRequired(module), origin += fuelRequired(module));   
+        }
+
+        static void Main(string[] args)
+        {
+            int fuelRequirements = 0;
+            Array.ConvertAll(System.IO.File.ReadAllLines("modules.txt"), x => int.Parse(x)).ToList().ForEach(x =>
+            {
+                fuelRequirements += GetFuelRequirement(x);
+            });
+
+            Console.WriteLine($"The amount of fuel required is {fuelRequirements}");
+            Console.ReadKey();
+        }
+    }
+}
